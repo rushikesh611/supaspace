5fdd263cccac75f1dfdbd31b7fce127a50cc4d04
Hrushikesh Jadhav <rushi611@gmail.com>
2023-01-07 20:15:18 +0530

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">supaspace</h1>
+    </div>
+  );
+};
+
+export default Home;
